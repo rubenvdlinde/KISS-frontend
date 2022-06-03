542bf4aaec02564af62586c021b0481e647a9fcf
mark stoker <mark@icatt.nl>
2022-06-03 16:01:26 +0200

    11proef geldig orgid

--- public/env.js
+++ public/env.js
@@ -1,3 +1,3 @@
 window.openPubBaseUri = "http://localhost/api/nieuws";
 window.contactmomentenBaseUri = "http://localhost/api/contactmomenten";
-window.organisatieIds = ["999990639"];
+window.organisatieIds = ["338135553"];
