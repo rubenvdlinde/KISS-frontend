84a7992c1ea4a98454dd3dff5ca3d17558411ed5
mark stoker <mark@icatt.nl>
2022-06-10 15:16:44 +0200

    gespreksresulataten config

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
-window.openPubBaseUri = "http://localhost/api/nieuws";
+window.openPubBaseUri = "https://kissdevelopment-dimpact.commonground.nu/api/openpub/kiss_openpub_pub";
 window.contactmomentenBaseUri = "http://localhost/api/contactmomenten";
-window.gespreksResultatenBaseUri = "http://localhost/api/ref/resultaattypeomschrijvingen";
+window.gespreksResultatenBaseUri = "https://kissdevelopment-dimpact.commonground.nu/api/ref/resultaattypeomschrijvingen";
 window.organisatieIds = ["998990639"];
