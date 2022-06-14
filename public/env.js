1244c982e14072df954129d82227601ed5eb1d0b
mark stoker <mark@icatt.nl>
2022-06-14 17:46:06 +0200

    merge issues

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
-window.openPubBaseUri = "https://kissdevelopment-dimpact.commonground.nu/api/openpub/kiss_openpub_pub";
+window.openPubBaseUri = "https://kissdevelopment-dimpact.commonground.nu/api/openpub";
 window.contactmomentenBaseUri = "http://localhost/api/contactmomenten";
 window.gespreksResultatenBaseUri = "https://kissdevelopment-dimpact.commonground.nu/api/ref/resultaattypeomschrijvingen";
 window.organisatieIds = ["999990639"];
