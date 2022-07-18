ddd4a0beb66c3ce4a1849cefaddf4610e0687af0
Felix Cornelissen <felix.cornelissen@icatt.nl>
2022-07-18 11:38:58 +0200

    cleanup

--- public/env.js
+++ public/env.js
@@ -1,4 +1,3 @@
-window.contactmomentenBaseUri = "http://localhost/api";
 window.organisatieIds = ["999990639"];
 window.pubBeheerUrl = "https://openweb.kiss-dev.commonground.nu/wp-login.php";
 window.gatewayBaseUri = "https://kissdevelopment-dimpact.commonground.nu";
