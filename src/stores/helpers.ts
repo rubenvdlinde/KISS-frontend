dbc5d04115f8bedc2beb4e07bd5861a8d4b136b2
Felix Cornelissen <felix.cornelissen@icatt.nl>
2022-07-25 17:40:50 +0200

    cleanup

--- src/stores/helpers.ts
+++ src/stores/helpers.ts
@@ -1,18 +1,12 @@
-import type { StoreDefinition, StateTree, _GettersTree } from "pinia";
+import type { StoreDefinition } from "pinia";
 import type { DeepReadonly } from "vue";
-
-type ReadonlyStoreDefinition<
-  S extends StateTree,
-  G extends _GettersTree<S>,
-  A
-> = () => DeepReadonly<S> & G & A;
 
 /**
  * Cast a store definition as readonly, so the only way to modify state is by using actions
  * @param storeDefinition
  */
-export function asReadOnly<S extends StateTree, G extends _GettersTree<S>, A>(
+export function asReadOnly<S, G, A>(
   storeDefinition: StoreDefinition<string, S, G, A>
 ) {
-  return storeDefinition as ReadonlyStoreDefinition<S, G, A>;
+  return storeDefinition as () => DeepReadonly<S> & G & A;
 }
