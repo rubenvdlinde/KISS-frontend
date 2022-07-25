8424a4bd5985664187c4aa544c90b4f50f452a51
Felix Cornelissen <felix.cornelissen@icatt.nl>
2022-07-25 17:02:39 +0200

    cleanup

--- src/stores/types.ts
+++ src/stores/types.ts
@@ -1,7 +1,11 @@
 import type { StoreDefinition } from "pinia";
 import type { DeepReadonly } from "vue";
 
-export type ReadonlyStoreDefinition<T extends StoreDefinition> =
-  T extends StoreDefinition<string, infer S, infer G, infer A>
+export type ReadonlyStoreDefinition<
+  T extends StoreDefinition,
+  K extends keyof ReturnType<T> | never = never
+> = T extends StoreDefinition<string, infer S, infer G, infer A>
+  ? K extends never
     ? () => DeepReadonly<S> & G & A
-    : never;
+    : () => Omit<DeepReadonly<S> & G & A, K>
+  : never;
