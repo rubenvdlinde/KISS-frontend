b3ff9890c2847f0fa3bcb6bdcf6d17fa3784073c
Felix Cornelissen <felix.cornelissen@icatt.nl>
2022-07-28 11:36:01 +0200

    fix logout

--- src/features/login/config.ts
+++ src/features/login/config.ts
@@ -1,9 +1,14 @@
 import type { RouteRecordRaw } from "vue-router";
 import RedirectPage from "./RedirectPage.vue";
 
+const baseUri = new URL(location.href);
+baseUri.pathname = "";
+const logoutUri = new URL(window.gatewayBaseUri + "/logout");
+logoutUri.searchParams.set("redirectUrl", baseUri.toString());
+
 export const meUrl = window.gatewayBaseUri + "/me";
 export const loginUrl = window.gatewayBaseUri + "/login/oidc/dex";
-export const logoutUrl = window.gatewayBaseUri + "/logout";
+export const logoutUrl = logoutUri.toString();
 export const redirectUrl = "/redirect-to-login";
 export const sessionStorageKey = "kiss_close";
 
