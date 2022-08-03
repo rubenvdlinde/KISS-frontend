ec6a621c03d2962e1cac362748836b166925efe0
Felix Cornelissen <felix.cornelissen@icatt.nl>
2022-08-03 10:14:48 +0200

    cleanup

--- src/features/klant/service.ts
+++ src/features/klant/service.ts
@@ -9,22 +9,6 @@ import {
 import type { Ref } from "vue";
 import type { Klant } from "@/stores/contactmoment";
 
-// er zijn endpoints voor het ophalen van:
-//  - een klant /api/klanten/{uuid}
-//  - contactmomenten van een klant /api/klantcontactmomenten
-// -  zaken bij contactmomenten /api/objectcontactmomenten
-
-//onduidelijk wat we nu moeten gebruiken voor het opbouwen van het klantbeeld
-//idiealiter alleen een call naar /api/klanten/{uuid} met ?extend[]=all
-//en dan in een keer alles binnenkrijgen. Kan dat?? zo ja, testdata svp
-
-//als dat niet kan, zouden we ook de andere calls kunnen gebruiken,
-//maar daar heb ik ook een paar vragen over:
-//in de docs voor het ophalen van klantcontactmometen staan een aantal queryparemeters als 'verplicht' (https://redocly.github.io/redoc/?nocors&url=https://kissdevelopment-dimpact.commonground.nu/openapi.json#tag/KlantContactMoment-contactmomenten-collection/operation/klantcontactmomenten%20KlantContactMoment_get)
-//klopt dat? krijg geen error als ik die niet gebruik
-//zit er nu geen testdata in of is de query fout? als ik hem toepas voor de testklant krijg ik niets terug:
-//https://kissdevelopment-dimpact.commonground.nu/api/klantcontactmomenten?klant.bronorganisatie=999990639&klant.klantnummer=00000221&klant.websiteUrl=/api/KlantAdres/96a1cbcd-3ac3-415f-a62b-0c5270bfae59&contactmoment.bronorganisatie=999990639&rol=gesprekspartner&extend[]=ContactMoment
-
 const isEmail = (val: string) =>
   val.match(
     /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
