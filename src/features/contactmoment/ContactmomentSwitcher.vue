8e3d1ffe5c02c435bf07d3ee5b3229b490bbf220
Felix Cornelissen <felix.cornelissen@icatt.nl>
2022-10-05 16:59:09 +0200

    fix button alignment

--- src/features/contactmoment/ContactmomentSwitcher.vue
+++ src/features/contactmoment/ContactmomentSwitcher.vue
@@ -110,6 +110,7 @@ menu {
     border-inline-start: 4px solid transparent;
     padding-inline-start: var(--spacing-small);
     inline-size: 100%;
+    text-align: inherit;
 
     &:hover {
       cursor: pointer;
