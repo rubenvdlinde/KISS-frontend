eb1beb7071ab6fb1f55ec9d93b43cafb30bade1a
Lencodes <contact@lencodes.com>
2022-10-06 11:50:18 +0200

    REFACTOR ZakenOverzicht handle select to upsert zaak

--- src/features/zaaksysteem/ZakenOverzicht.vue
+++ src/features/zaaksysteem/ZakenOverzicht.vue
@@ -37,12 +37,21 @@
 import { formatDateOnly } from "@/helpers/date";
 import type { PropType } from "vue";
 import type { Zaak } from "./types";
-import type { Vraag } from "@/stores/contactmoment";
+import { useContactmomentStore, type Vraag } from "@/stores/contactmoment";
+import { useRouter } from "vue-router";
 
-const emit = defineEmits(["zaakSelected"]);
+const router = useRouter();
+const contactmomentStore = useContactmomentStore();
 
 const handleZaakSelected = (zaak: Zaak) => {
-  emit("zaakSelected", zaak);
+  if (!contactmomentStore.huidigContactmoment) return;
+
+  contactmomentStore.upsertZaak(
+    zaak,
+    contactmomentStore.huidigContactmoment.huidigeVraag
+  );
+
+  router.push({ name: "zaakDetail", params: { zaakId: zaak.id } });
 };
 
 defineProps({
