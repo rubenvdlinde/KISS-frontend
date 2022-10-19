5ad6768608c32ffd9c2933ce0b00febe672f6caa
Felix Cornelissen <felix.cornelissen@icatt.nl>
2022-10-19 10:22:13 +0200

    ADD skeleton in stead of spinner

--- src/features/klant/KlantenOverzichtRow.vue
+++ src/features/klant/KlantenOverzichtRow.vue
@@ -1,17 +1,17 @@
 <template>
   <tr class="row-link">
     <th scope="row" class="wrap">
-      <simple-spinner class="spinner" v-if="result.naam.loading" />
+      <div class="skeleton" v-if="result.naam.loading" />
       <template v-else-if="result.naam.success">{{
         result.naam.data
       }}</template>
     </th>
     <td class="wrap">
-      <simple-spinner class="spinner" v-if="result.emails.loading" />
+      <div class="skeleton" v-if="result.emails.loading" />
       <template v-if="result.emails.success">{{ result.emails.data }}</template>
     </td>
     <td class="wrap">
-      <simple-spinner class="spinner" v-if="result.telefoonnummers.loading" />
+      <div class="skeleton" v-if="result.telefoonnummers.loading" />
       <template v-if="result.telefoonnummers.success">{{
         result.telefoonnummers.data
       }}</template>
@@ -20,23 +20,20 @@
       {{ result.bsn }}
     </td>
     <td>
-      <simple-spinner class="spinner" v-if="result.geboortedatum.loading" />
+      <div class="skeleton" v-if="result.geboortedatum.loading" />
       <dutch-date
         v-else-if="result.geboortedatum.success && result.geboortedatum.data"
         :date="result.geboortedatum.data"
       />
     </td>
     <td>
-      <simple-spinner
-        class="spinner"
-        v-if="result.postcodeHuisnummer.loading"
-      />
+      <div class="skeleton" v-if="result.postcodeHuisnummer.loading" />
       <template v-if="result.postcodeHuisnummer.success">{{
         result.postcodeHuisnummer.data
       }}</template>
     </td>
     <td>
-      <simple-spinner class="spinner" v-if="result.detailLink.loading" />
+      <div class="skeleton" v-if="result.detailLink.loading" />
       <template v-if="result.detailLink.success">
         <router-link
           v-if="result.detailLink.data"
@@ -54,20 +51,17 @@
 </template>
 <script lang="ts" setup>
 import type { EnrichedPersoon } from "./types";
-import SimpleSpinner from "@/components/SimpleSpinner.vue";
 import DutchDate from "@/components/DutchDate.vue";
 
 defineProps<{ result: EnrichedPersoon }>();
 </script>
 
 <style scoped lang="scss">
-td {
-  > .spinner {
-    font-size: inherit;
-  }
+td:empty::after {
+  content: "-";
+}
 
-  &:empty::after {
-    content: "-";
-  }
+.skeleton {
+  min-height: 1rem;
 }
 </style>
