c2e78319357e7c1aee8eaf76dc3e52bc0be1cc4d
Pascal van Oostenbrugge <pascal@icatt.nl>
2023-01-22 11:05:57 +0100

    revert env

--- public/env.js
+++ public/env.js
@@ -1,3 +1,3 @@
 window.organisatieIds = ["999990639"];
-window.pubBeheerUrl = "https://openweb.dev.kiss-demo.nl/wp-login.php";
-window.gatewayBaseUri = "https://dev.kiss-demo.nl";
+window.pubBeheerUrl = "https://openweb.kiss.commonground.nu/wp-login.php";
+window.gatewayBaseUri = "https://kissdevelopment.commonground.nu";
